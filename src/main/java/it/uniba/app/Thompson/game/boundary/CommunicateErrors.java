81bcae8a4ff84c41217d14dea91dd219deb10721
f1r3k3rn <cronanacron@gmail.com>
2024-05-16 19:31:01 +0200

    feat(CommunicateErrors): Creazione classe CommunicateErrors

---
+++ src/main/java/it/uniba/app/Thompson/game/boundary/CommunicateErrors.java
@@ -0,0 +1,26 @@
+package main.java.it.uniba.app.Thompson.game.boundary;
+import main.java.it.uniba.app.Thompson.game.util.ErrorMessages;
+/**
+ * << Boundary >>
+ * Class to communicate errors.
+ */
+public final class CommunicateErrors {
+    /**
+     * Constructor for CommunicateErrors.
+     */
+    private CommunicateErrors() { }
+    /**
+     * Print invalid command.
+     * Print to stdout the invalid command message.
+     */
+    public void printInvalidCommand() {
+        System.out.println(ErrorMessages.INVALID_COMMAND);
+    }
+    /**
+     * Print invalid input.
+     * Print to stdout the invalid input message.
+     */
+    public void printInvalidInput() {
+        System.out.println(ErrorMessages.INVALID_INPUT);
+    }
+}
