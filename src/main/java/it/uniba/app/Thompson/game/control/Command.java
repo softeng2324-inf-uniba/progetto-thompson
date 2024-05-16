d7217368f4007d816a16408d91061bc7fd83bc2b
f1r3k3rn <cronanacron@gmail.com>
2024-05-16 19:33:48 +0200

    fix(Command): Correzione path package e javadoc

--- src/main/java/it/uniba/app/Thompson/game/control/Command.java
+++ src/main/java/it/uniba/app/Thompson/game/control/Command.java
@@ -1,25 +1,21 @@
-package it.uniba.app.Thompson.game.control;
-
-import it.uniba.app.Thompson.game.util.CommandStatus;
-
+package main.java.it.uniba.app.Thompson.game.control;
+import main.java.it.uniba.app.Thompson.game.util.CommandStatus;
 /**
  * << Control >>
  * Controls for command constants.
  */
 public abstract class Command {
     private String command;
-
     /**
      * Method that returns command.
-     * @return
+     * @return Name of the command.
      */
     String getCommand() {
         return command;
     }
-
     /**
      * Method that executes some instructions and return a CommandStatus.
-     * @return
+     * @return Instance of CommandStatus.
      */
     CommandStatus executeCommand() {
         return CommandStatus.SUCCESSFUL;
