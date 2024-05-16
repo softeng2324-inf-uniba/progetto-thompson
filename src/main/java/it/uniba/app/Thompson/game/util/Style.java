d8ae8b595f46957a204762e69ca12918db92d398
Pascoooo <pacucciniko@gmail.com>
2024-05-16 16:44:55 +0200

    feat(Style) : Aggiunta classe per le costanti Unicode dei fonts

---
+++ src/main/java/it/uniba/app/Thompson/game/util/Style.java
@@ -0,0 +1,17 @@
+package it.uniba.app.Thompson.game.util;
+/**
+ Util
+ Class for font constants.
+ */
+public final class Style {
+    /**
+     * Constants for font management.
+     */
+    public static final String BOLD = "\u001B[1m";
+    public static final String ITALIC = "\u001B[3m";
+    public static final String NORMAL = "\u001B[0m";
+    /**
+     * Style constructor.
+     */
+    private Style() { }
+}
