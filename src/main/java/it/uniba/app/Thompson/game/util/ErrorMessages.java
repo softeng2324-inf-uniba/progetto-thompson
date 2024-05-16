4ea369a882b3fbdd02e6a699056c43c0daa9a053
f1r3k3rn <cronanacron@gmail.com>
2024-05-16 19:30:38 +0200

    feat(ErrorMessages): Creazione classe ErrorMessages

---
+++ src/main/java/it/uniba/app/Thompson/game/util/ErrorMessages.java
@@ -0,0 +1,16 @@
+package main.java.it.uniba.app.Thompson.game.util;
+/**
+ * << Util >>
+ * Class for color constants.
+ */
+public final class ErrorMessages {
+    /**
+     * Constants for error messages.
+     */
+    public static final String INVALID_COMMAND = "Comando non valido,riprova.";
+    public static final String INVALID_INPUT = "Input non valido,riprova.";
+    /**
+     * Constructor for ErrorMessages.
+     */
+    private ErrorMessages() { }
+}
