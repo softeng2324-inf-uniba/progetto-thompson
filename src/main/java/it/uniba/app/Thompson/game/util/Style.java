aee9145230564d27ffde285eb84788453bb91a22
Pascoooo <pacucciniko@gmail.com>
2024-05-16 18:07:49 +0200

    feat(Style) : Aggiunta enumerativi per gli stili disponibili

--- src/main/java/it/uniba/app/Thompson/game/util/Style.java
+++ src/main/java/it/uniba/app/Thompson/game/util/Style.java
@@ -1,17 +1,10 @@
 package it.uniba.app.Thompson.game.util;
+
 /**
- Util
- Class for font constants.
+ * Enum with implemented text styles.
  */
-public final class Style {
-    /**
-     * Constants for font management.
-     */
-    public static final String BOLD = "\u001B[1m";
-    public static final String ITALIC = "\u001B[3m";
-    public static final String NORMAL = "\u001B[0m";
-    /**
-     * Style constructor.
-     */
-    private Style() { }
+public enum Style {
+    BOLD,
+    ITALIC,
+    NORMAL
 }
