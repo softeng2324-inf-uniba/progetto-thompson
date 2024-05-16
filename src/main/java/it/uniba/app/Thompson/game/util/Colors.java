5f1a44df8bc7e6f73dccaf4021802c7fa47e5809
Pascoooo <pacucciniko@gmail.com>
2024-05-16 16:15:39 +0200

    feat(Colors) : Aggiunta classe per le costanti Unicode dei colori

---
+++ src/main/java/it/uniba/app/Thompson/game/util/Colors.java
@@ -0,0 +1,18 @@
+package it.uniba.app.Thompson.game.util;
+/**
+ * Util
+ * Class for color constants.
+ */
+public final class Colors {
+    /**
+     * Constants for color management.
+     */
+    public static final String YELLOW = "\u001B[33m";
+    public static final String ORANGE = "\u001B[38;5;208m";
+    public static final String PINK = "\u001B[38;5;206m";
+    public static final String GREY = "\u001B[90m";
+    /**
+     * Color constructor.
+     */
+    private Colors() { }
+}
