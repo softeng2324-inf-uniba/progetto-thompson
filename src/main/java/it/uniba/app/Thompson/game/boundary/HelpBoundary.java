e6b4d9a794c2791cce4caa116fc753e128c3d895
frasga <s.francesco297@gmail.com>
2024-05-17 12:03:12 +0200

    feat(HelpBoundary): Creazione HelpBoundary per stampare i comandi disponibili

---
+++ src/main/java/it/uniba/app/Thompson/game/boundary/HelpBoundary.java
@@ -0,0 +1,19 @@
+package it.uniba.app.Thompson.game.boundary;
+import it.uniba.app.Thompson.game.control.HelpCommandControl;
+
+/**
+ * << Boundary >>
+ * Class used to print available commands
+ */
+public class HelpBoundary {
+
+    /**
+     * Print available commands
+     */
+    public static void printCommands() {
+        System.out.println(
+            "COMANDI DISPONIBILI \n" +
+            " - " + HelpCommandControl.getInstance().getCommand() + " : " + HelpCommandControl.getInstance().getDescription() + "\n"
+        );
+    }
+}
