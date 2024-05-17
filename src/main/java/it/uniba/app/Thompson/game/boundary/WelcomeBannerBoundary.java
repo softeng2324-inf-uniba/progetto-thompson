a0bccc8129f6ea19c8e0f11968c334baa56ea2ce
Pascoooo <pacucciniko@gmail.com>
2024-05-17 10:15:21 +0200

    ref(WelcomeBannerBoundary): Aggiunta colori banner

--- src/main/java/it/uniba/app/Thompson/game/boundary/WelcomeBannerBoundary.java
+++ src/main/java/it/uniba/app/Thompson/game/boundary/WelcomeBannerBoundary.java
@@ -1,5 +1,9 @@
 package it.uniba.app.Thompson.game.boundary;
 
+import it.uniba.app.Thompson.game.control.FormatterControl;
+import it.uniba.app.Thompson.game.util.Color;
+import it.uniba.app.Thompson.game.util.Style;
+
 /**
  * << Boundary >>
  * Class to print the welcome banner.
@@ -8,25 +12,39 @@ public final class WelcomeBannerBoundary {
     /**
      * Banner to print.
      */
-    private static final String[] banner = new String[]{
-        "    // | |  /__  ___/ // | |  \\ \\/ /   \\ \\/ / ",
-        "   //__| |    / /    //__| |   \\/ /     \\/ /    ",
-        "  // __| |   / /    / ___  |   / /\\     / /\\    ",
-        " //    | |  / /    //    | |  / /\\ \\   / /\\ \\ ",
-        "//     | | / /    //     | | / /  \\ \\ / /  \\ \\",
-    };
+    private String[] banner;
+
     /**
      * Constructor for WelcomeBanner.
      */
-    private WelcomeBannerBoundary() { }
+    public WelcomeBannerBoundary() {
+        initBanner(new FormatterControl());
+    }
+
     /**
      * Print banner.
      * Print the banner to stdout.
      */
-    public static void printBanner() {
+    public void printBanner() {
         for (String i : banner) {
             System.out.println(i);
         }
         System.out.println("Benvenuti, questo è il gioco del gruppo Thompson!!");
+    }
+
+    /**
+     * Initialize the banner.
+     * @param fc FormatterControl
+     */
+    void initBanner(final FormatterControl fc) {
+        this.banner = new String[]{
+            fc.formatText("------------------------------------------------", Color.RED, Style.BOLD),
+            fc.formatText("-    // | |  /__  ___/ // | |  \\ \\/ /   \\ \\/ / -", Color.RED, Style.BOLD),
+            fc.formatText("-   //__| |    / /    //__| |   \\/ /     \\/ /  -", Color.RED, Style.BOLD),
+            fc.formatText("-  // __| |   / /    / ___  |   / /\\     / /\\  -", Color.RED, Style.BOLD),
+            fc.formatText("- //    | |  / /    //    | |  / /\\ \\   / /\\ \\ -", Color.BLUE, Style.BOLD),
+            fc.formatText("-//     | | / /    //     | | / /  \\ \\ / /  \\ \\-", Color.BLUE, Style.BOLD),
+            fc.formatText("------------------------------------------------", Color.BLUE, Style.BOLD),
+        };
     }
 }
