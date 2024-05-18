e70816b4e8516f499bf53bd73bae716556a8c3ac
f1r3k3rn <cronanacron@gmail.com>
2024-05-18 18:02:33 +0200

    feat(VoidCommandControl): Creazione della classe VoidCommandControl.

---
+++ src/main/java/it/uniba/app/Thompson/game/control/VoidCommandControl.java
@@ -0,0 +1,56 @@
+package it.uniba.app.Thompson.game.control;
+import it.uniba.app.Thompson.game.boundary.PrintBoardBoundary;
+import it.uniba.app.Thompson.game.entity.Board;
+import it.uniba.app.Thompson.game.util.CommandStatus;
+
+/**
+ * << Control >>
+ * Implementation of the play command.
+ */
+public final class VoidCommandControl extends CommandControl {
+
+    private static final String COMMAND = "/vuoto";
+    private static final String DESCRIPTION = "Stampa del tavoliere vuoto.";
+    private static final VoidCommandControl INSTANCE = new VoidCommandControl();
+
+    /**
+     * Constructor for VoidCommandControl.
+     */
+    private VoidCommandControl() { }
+
+    /**
+     * @return The instance of the VoidCommandControl.
+     */
+    public static VoidCommandControl getInstance() {
+        return INSTANCE;
+    }
+
+    /**
+     * Override of the getCommand method of the abstract class CommandControl.
+     * @return The command string.
+     */
+    @Override
+    public String getDescription() {
+        return DESCRIPTION;
+    }
+
+    /**
+     * Override of the getCommand method of the abstract class CommandControl.
+     * @return The command string.
+     */
+    @Override
+    public String getCommand() {
+        return COMMAND;
+    }
+
+    /**
+     * Override of the executeCommand method of the abstract class CommandControl.
+     * @return The status of the command.
+     */
+    @Override
+    CommandStatus executeCommand() {
+        Board board = new Board(false);
+        PrintBoardBoundary.printBoard(board);
+        return CommandStatus.SUCCESSFUL;
+    }
+}
