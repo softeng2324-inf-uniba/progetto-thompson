1d049675c6c705c75e09ac65a5fc2635f194faf1
f1r3k3rn <cronanacron@gmail.com>
2024-05-18 18:03:29 +0200

    ref(HelpBoundary): Aggiunto comando VoidCommandControl.

--- src/main/java/it/uniba/app/Thompson/game/boundary/HelpBoundary.java
+++ src/main/java/it/uniba/app/Thompson/game/boundary/HelpBoundary.java
@@ -2,6 +2,7 @@ package it.uniba.app.Thompson.game.boundary;
 import it.uniba.app.Thompson.game.control.ExitCommandControl;
 import it.uniba.app.Thompson.game.control.HelpCommandControl;
 import it.uniba.app.Thompson.game.control.PlayCommandControl;
+import it.uniba.app.Thompson.game.control.VoidCommandControl;
 
 /**
  * << Boundary >>
@@ -21,6 +22,8 @@ public final class HelpBoundary {
                     + ExitCommandControl.getInstance().getDescription() + "\n"
             + " - " + PlayCommandControl.getInstance().getCommand() + " : "
                     + PlayCommandControl.getInstance().getDescription() + "\n"
+            + " - " + VoidCommandControl.getInstance().getCommand() + " : "
+                    + VoidCommandControl.getInstance().getDescription() + "\n"
         );
 
     }
