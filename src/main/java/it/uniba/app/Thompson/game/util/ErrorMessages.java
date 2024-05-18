5758fd077ba3dcc1a732b1abe8478535e0b3d094
MikiMik88 <michelepontrelli8@gmail.com>
2024-05-18 19:03:47 +0200

    ref(ErrorMessages): Aggiunti nuovi messaggi di errore

--- src/main/java/it/uniba/app/Thompson/game/util/ErrorMessages.java
+++ src/main/java/it/uniba/app/Thompson/game/util/ErrorMessages.java
@@ -12,6 +12,9 @@ public final class ErrorMessages {
     public static final String INVALID_COMMAND = "Comando non valido,riprova.";
     public static final String INVALID_INPUT = "Input non valido,riprova.";
     public static final String MATCH_NOT_NULL = "C'è già una partita in corso.";
+    public static final String MATCH_NULL = "Non c'è nessuna partita in corso.";
+    public static final String INVALID_PLAYER = "Il giocatore inserito non è valido, comando annullato.";
+    public static final String INVALID_CHOICE = "Scelta non valida, comando annullato";
 
     /**
      * Constructor for ErrorMessages.
