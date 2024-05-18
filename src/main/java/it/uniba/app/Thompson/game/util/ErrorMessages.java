c4c0cddcef70f3b6489fe3f9cce1140767619566
IXRyu <fabiozippo03@hotmail.com>
2024-05-18 15:18:12 +0200

    ref(ErrorMessages): Aggiunta errore di partita già iniziata

--- src/main/java/it/uniba/app/Thompson/game/util/ErrorMessages.java
+++ src/main/java/it/uniba/app/Thompson/game/util/ErrorMessages.java
@@ -5,11 +5,14 @@ package it.uniba.app.Thompson.game.util;
  * Class for color constants.
  */
 public final class ErrorMessages {
+
     /**
      * Constants for error messages.
      */
     public static final String INVALID_COMMAND = "Comando non valido,riprova.";
     public static final String INVALID_INPUT = "Input non valido,riprova.";
+    public static final String MATCH_NOT_NULL = "C'è già una partita in corso.";
+
     /**
      * Constructor for ErrorMessages.
      */
