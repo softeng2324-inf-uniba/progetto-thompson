ab3c3a288faaa85160f72abb52c0a6bbbed128ea
MikiMik88 <michelepontrelli8@gmail.com>
2024-05-18 16:48:21 +0200

    ref(PrintBoardBoundary): versione di test del metodo printBoard

--- src/main/java/it/uniba/app/Thompson/game/boundary/PrintBoardBoundary.java
+++ src/main/java/it/uniba/app/Thompson/game/boundary/PrintBoardBoundary.java
@@ -1,6 +1,7 @@
 package it.uniba.app.Thompson.game.boundary;
 import it.uniba.app.Thompson.game.entity.Board;
 import it.uniba.app.Thompson.game.util.Coordinate;
+import it.uniba.app.Thompson.game.util.UnicodePawn;
 
 public final class PrintBoardBoundary {
     private PrintBoardBoundary() { }
@@ -9,20 +10,19 @@ public final class PrintBoardBoundary {
         int size = board.getSize();
         StringBuilder lol = new StringBuilder();
 
+        System.out.println("   A  B  C  D  E  F  G");
         for (int i = 0; i < size; i++) {
-            System.out.println("POP");
             lol.setLength(0);
-            System.out.println("POP");
-            lol.append(i);
-            System.out.println("POP");
-            lol.append(" ");
-            System.out.println("POP");
+            lol.append(i + 1);
+            lol.append(String.format("%3s", "|"));
+            System.out.println("___________________________________");
             for (int j = 0; j < size; j++) {
                 if (board.getTile(new Coordinate(i, j)).isOccupied()) {
-                    lol.append(board.getTile(new Coordinate(i, j)).getPawn().getFigure());
+                    lol.append(UnicodePawn.getPawnUnicode(board.getTile(new Coordinate(i, j)).getPawn().getFigure()));
                 } else {
-                    lol.append(" ");
+                    lol.append(String.format("%3s", " "));
                 }
+                lol.append(String.format("%3s", "|"));
             }
             System.out.println(lol.toString());
         }
