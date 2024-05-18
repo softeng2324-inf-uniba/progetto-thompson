5d474c33292729633a9ad1e4e9138598ad6fc557
IXRyu <fabiozippo03@hotmail.com>
2024-05-18 15:23:36 +0200

    feat(Board): Aggiunta funzionalità di riempimento del tavoliere con le pedine iniziali

--- src/main/java/it/uniba/app/Thompson/game/entity/Board.java
+++ src/main/java/it/uniba/app/Thompson/game/entity/Board.java
@@ -1,4 +1,5 @@
 package it.uniba.app.Thompson.game.entity;
+import it.uniba.app.Thompson.game.util.PawnFigure;
 import it.uniba.app.Thompson.game.util.Coordinate;
 
 /**
@@ -17,21 +18,34 @@ public final class Board {
     /**
      * Default Constructor for the class Board.
      */
-    private Board() {
+    public Board(final boolean isGameBoard) {
         size = DEFAULT_SIZE;
+        initBoard(isGameBoard);
     }
 
     /**
      * Constructor for the class Board with a parameter characterizing the size of the board.
      * @param newSize size.
      */
-    private Board(final int newSize) {
+    public Board(final boolean isGameBoard, final int newSize) {
         size = newSize;
+        initBoard(isGameBoard);
+    }
+
+    /**
+     * Method setInitialPawns.
+     * Places the initial pawns on the board.
+     */
+    public void setInitialPawns() {
+        tiles[0].placePawn(PawnFigure.BLACK_PAWN);
+        tiles[size ^ 2 - 1].placePawn(PawnFigure.BLACK_PAWN);
+        tiles[size - 1].placePawn(PawnFigure.WHITE_PAWN);
+        tiles[size * (size - 1)].placePawn(PawnFigure.WHITE_PAWN);
     }
 
     /**
      * Method getSize.
-     * @return size.
+     * @return size The size of the board.
      */
     public int getSize() {
         return size;
@@ -41,19 +55,22 @@ public final class Board {
      * Method initBoard
      * Creates the board.
      */
-    private void initBoard() {
+    private void initBoard(final boolean isGameBoard) {
         tiles = new Tile[size ^ 2];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 tiles[i * size + j] = new Tile(i, j);
             }
         }
+        if (isGameBoard) {
+            setInitialPawns();
+        }
     }
 
     /**
      * Method getTile.
-     * @param coordinate the coordinate.
-     * @return the tile.
+     * @param coordinate The coordinate.
+     * @return The tile.
      */
     public Tile getTile(final Coordinate coordinate) {
         return tiles[coordinate.getX() * size + coordinate.getY()];
