3c75dbb12525360960cc6e162c31903392f1faa3
Pascoooo <pacucciniko@gmail.com>
2024-05-18 11:28:02 +0200

    ref(Player): Modifica classe Player

--- src/main/java/it/uniba/app/Thompson/game/entity/Player.java
+++ src/main/java/it/uniba/app/Thompson/game/entity/Player.java
@@ -2,7 +2,7 @@ package it.uniba.app.Thompson.game.entity;
 import it.uniba.app.Thompson.game.util.Color;
 
 /**
- * <<Entity>>
+ * << Entity >>
  * Class Player.
  */
 public final class Player {
@@ -13,32 +13,42 @@ public final class Player {
     private final Color pawnColor;
 
     /**
-     * Method to set the number of pawns.
-     * @param numPedine number of pawns
+     * Constructor for the class Player.
      */
-    private void setNumeroPedine(final int numPedine) {
-        this.numeroPedine = numPedine;
+    private Player(final Color color) {
+        pawnColor = color;
     }
 
     /**
      * Method to get the color of the pawns.
      * @return the color of the pawns
      */
-    public Color getColorePedine() {
-        return COLOREPEDINE;
+    public Color getColor() {
+        return pawnColor;
     }
 
     /**
      * Method to invalidate a cell.
      * @param tile the cell to invalidate
      */
-    public void invalidateCasella(final Tile tile) {
+    public void invalidateTile(final Tile tile) {
         tile.setInvalid(true);
     }
 
+    /*
     /**
-     * Costructor Player.
-     */
-    private Player() {
+     * Method to get the number of pawns.
+     * @return the number of pawns
+     * /
+    public int getPawnNumber() {
+        return ;
     }
+    /**
+     * Method to set the number of pawns.
+     * @param numPedine number of pawns
+     * /
+    private void setPawnNumber(final int numPedine) {
+        this.numeroPedine = numPedine;
+    }
+    */
 }
