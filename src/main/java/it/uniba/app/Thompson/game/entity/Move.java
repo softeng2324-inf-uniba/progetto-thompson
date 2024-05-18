7fb6570f306aa9277d91dd0352725760d6c3720a
Pascoooo <pacucciniko@gmail.com>
2024-05-18 11:23:18 +0200

    ref(Move): Cambiamento classe Move

--- src/main/java/it/uniba/app/Thompson/game/entity/Move.java
+++ src/main/java/it/uniba/app/Thompson/game/entity/Move.java
@@ -10,48 +10,16 @@ public final class Move {
     /**
      * Attributes of Move.
      */
-    @SuppressWarnings("FieldCanBeLocal")
-    private MoveTypes moveTypes;
-    @SuppressWarnings("FieldCanBeLocal")
-    private char letter;
-    @SuppressWarnings("FieldCanBeLocal")
-    private char number;
-    @SuppressWarnings("FieldCanBeLocal")
-    private final boolean invalid = false;
+    private final Coordinate from;
+    private final Coordinate to;
 
     /**
-     * Method to set the letter.
-     * @param charLetter the letter
+     * Constructor for the class Move.
+     * @param start the start coordinate
+     * @param end the end coordinate
      */
-    public void setLetter(final char charLetter) {
-        this.letter = charLetter;
-    }
-
-    /**
-     * Method to set the number.
-     * @param setNumber the number
-     */
-    public void setNumber(final char setNumber) {
-        this.number = setNumber;
-    }
-
-    /**
-     * Method to set the move type.
-     * @param typeOfMove the type of move
-     */
-    public void setMoveTypes(final MoveTypes typeOfMove) {
-        this.moveTypes = typeOfMove;
-    }
-
-    /**
-     * Method to see if the move is valid.
-     * @return boolean
-     */
-    public boolean isMoveValid() {
-        return !invalid;
-    }
-
-    private Move() {
-
+    private Move(final Coordinate start, final Coordinate end) {
+        from = start;
+        to = end;
     }
 }
