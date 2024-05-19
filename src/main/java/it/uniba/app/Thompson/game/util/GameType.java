1f035d76889f568c065d5e7fdca6776e805e2d04
f1r3k3rn <cronanacron@gmail.com>
2024-05-19 13:59:28 +0200

    feat(GameType): Aggiunta classe GameType

---
+++ src/main/java/it/uniba/app/Thompson/game/util/GameType.java
@@ -0,0 +1,12 @@
+package it.uniba.app.Thompson.game.util;
+
+/**
+ *  << Util >>
+ *  Enum for the game type.
+ */
+public enum  GameType {
+    DEFAULT,
+    THOMAS,
+    INFECTION,
+    A_PERDERE
+}
