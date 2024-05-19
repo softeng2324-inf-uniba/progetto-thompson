51690a49f5ecb3cfbea97090e7eaa2e2f0e1097e
Pascoooo <pacucciniko@gmail.com>
2024-05-19 17:00:01 +0200

    ref(VariantMove): Risoluzione errore SpotBugs

--- src/main/java/it/uniba/app/Thompson/game/util/VariantMove.java
+++ src/main/java/it/uniba/app/Thompson/game/util/VariantMove.java
@@ -1,35 +1,44 @@
 package it.uniba.app.Thompson.game.util;
 
+import java.util.Arrays;
+
 /**
  * <<Util>>
  * Enum for the variant moves for game types.
  */
 public final class VariantMove {
 
-    public static final Coordinate[][] STANDARD = {
-        {
-            new Coordinate(-1, -1), new Coordinate(-1, 0), new Coordinate(-1, 1),
-            new Coordinate(0, -1), new Coordinate(0, 1),
-            new Coordinate(1, -1), new Coordinate(1, 0), new Coordinate(1, 1),
+    private static final Coordinate[][] STANDARD = {
+            {
+                    new Coordinate(-1, -1), new Coordinate(-1, 0), new Coordinate(-1, 1),
+                    new Coordinate(0, -1), new Coordinate(0, 1),
+                    new Coordinate(1, -1), new Coordinate(1, 0), new Coordinate(1, 1),
 
-        },
-        {
-            new Coordinate(-2, -2), new Coordinate(-2, -1),
-            new Coordinate(-2, 0), new Coordinate(-2, 1),
-            new Coordinate(-2, 2), new Coordinate(-1, -2),
-            new Coordinate(0, -2), new Coordinate(0, 2),
-            new Coordinate(1, -2), new Coordinate(1, 2),
-            new Coordinate(2, -2), new Coordinate(2, -1),
-            new Coordinate(2, 0), new Coordinate(2, 1),
-            new Coordinate(2, 2), new Coordinate(-1, 2),
-        }
+            },
+            {
+                    new Coordinate(-2, -2), new Coordinate(-2, -1),
+                    new Coordinate(-2, 0), new Coordinate(-2, 1),
+                    new Coordinate(-2, 2), new Coordinate(-1, -2),
+                    new Coordinate(0, -2), new Coordinate(0, 2),
+                    new Coordinate(1, -2), new Coordinate(1, 2),
+                    new Coordinate(2, -2), new Coordinate(2, -1),
+                    new Coordinate(2, 0), new Coordinate(2, 1),
+                    new Coordinate(2, 2), new Coordinate(-1, 2),
+            }
     };
-
 
     /**
      * Constructor for the class VariantMove.
      */
     private VariantMove() {
 
+    }
+
+    /**
+     * Method getStandard.
+     * @return STANDARD The standard variant moves
+     */
+    public static Coordinate[][] getStandard() {
+        return Arrays.copyOf(STANDARD, STANDARD.length);
     }
 }
