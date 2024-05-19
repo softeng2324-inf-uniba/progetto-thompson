8178bf5042d6eca7ab74775355dd712b7b9e724a
MikiMik88 <michelepontrelli8@gmail.com>
2024-05-19 10:31:15 +0200

    ref(ErrorMessages): Correzione JavaDoc e SUGGEST_MATCH_INIT

--- src/main/java/it/uniba/app/Thompson/game/util/ErrorMessages.java
+++ src/main/java/it/uniba/app/Thompson/game/util/ErrorMessages.java
@@ -2,21 +2,21 @@ package it.uniba.app.Thompson.game.util;
 
 /**
  * << Util >>
- * Class for color constants.
+ * Class for error messages.
  */
 public final class ErrorMessages {
 
     /**
      * Constants for error messages.
      */
-    public static final String INVALID_COMMAND = "Comando non valido,riprova.";
-    public static final String INVALID_INPUT = "Input non valido,riprova.";
+    public static final String INVALID_COMMAND = "Comando non valido, riprova.";
+    public static final String INVALID_INPUT = "Input non valido, riprova.";
     public static final String MATCH_NOT_NULL = "C'è già una partita in corso.";
-    public static final String MATCH_NULL = "Non c'è nessuna partita in corso";
+    public static final String MATCH_NULL = "Non c'è nessuna partita in corso.";
     public static final String INVALID_PLAYER = "Il giocatore inserito non è valido, comando annullato.";
-    public static final String INVALID_CHOICE = "Scelta non valida, comando annullato";
-    public static final String SUGGEST_MATCH_INIT = MATCH_NULL
-            + ", utilizza '/gioca' per iniziarne una.";
+    public static final String INVALID_CHOICE = "Scelta non valida, comando annullato.";
+    public static final String SUGGEST_MATCH_INIT = "Non c'è nessuna partita in corso,"
+                                                  + "utilizza '/gioca' per iniziarne una.";
 
     /**
      * Constructor for ErrorMessages.
