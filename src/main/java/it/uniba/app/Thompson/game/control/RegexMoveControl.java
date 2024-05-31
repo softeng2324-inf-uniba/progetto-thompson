d1d2d6b456374bfb871215ed11c04f5e644ba398
IXRyu <fabiozippo03@hotmail.com>
2024-05-31 20:05:16 +0200

    ref(RegexMoveControl): Aggiunta Javadoc

--- src/main/java/it/uniba/app/Thompson/game/control/RegexMoveControl.java
+++ src/main/java/it/uniba/app/Thompson/game/control/RegexMoveControl.java
@@ -2,20 +2,37 @@ package it.uniba.app.Thompson.game.control;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
+/**
+ * {@literal << Control >>}
+ * Class to manage the regular expressions for the movement control.
+ */
 public class RegexMoveControl {
-    private final String movement = "[a-g|A-G][1-7]-[a-g|A-G][1-7]";
-    private final String block    = "[a-g|A-G][1-7]";
+    private static final String movement = "[a-g|A-G][1-7]-[a-g|A-G][1-7]";
+    private static final String block    = "[a-g|A-G][1-7]";
 
+    /**
+     * Constructor for the class RegexMoveControl.
+     */
     public RegexMoveControl() {
     }
 
-    public boolean ControlInputMovement(String input) {
+    /**
+     * Method controlInputMovement.
+     * @param input The input of the movement
+     * @return matcher The boolean value of the movement
+     */
+    public boolean controlInputMovement(final String input) {
         Pattern pattern = Pattern.compile(movement);
         Matcher matcher = pattern.matcher(input);
         return matcher.matches();
     }
 
-    public boolean ControlInputBlock(String input) {
+    /**
+     * Method controlInputBlock.
+     * @param input The input of the block
+     * @return matcher The boolean value of the block
+     */
+    public boolean controlInputBlock(final String input) {
         Pattern pattern = Pattern.compile(block);
         Matcher matcher = pattern.matcher(input);
         return matcher.matches();
