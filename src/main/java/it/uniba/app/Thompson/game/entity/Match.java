dd53b2d38e08ce290e0888d3fcfcb334f97cd222
Pascoooo <pacucciniko@gmail.com>
2024-05-31 17:30:46 +0200

    feat(Match) : Aggiunta attributi e metodi per inizializzare e formattare il tempo trascorso dall'inizio della partita

--- src/main/java/it/uniba/app/Thompson/game/entity/Match.java
+++ src/main/java/it/uniba/app/Thompson/game/entity/Match.java
@@ -1,4 +1,5 @@
 package it.uniba.app.Thompson.game.entity;
+
 import java.util.Stack;
 
 /**
@@ -13,12 +14,17 @@ public class Match {
     private final Stack<Move> moves = new Stack<>();
     private final Board board;
     private static final boolean IS_GAME_BOARD = true;
+    private static String startTime;
+    private static String elapsedTime;
+    private static final int SECONDS = 60;
+    private static final int MILLIS = 1000;
 
     /**
      * Constructor for the class Match.
      */
     public Match() {
         board = new Board(IS_GAME_BOARD);
+        startTime = String.valueOf(System.currentTimeMillis());
     }
 
     /**
@@ -55,5 +61,28 @@ public class Match {
         Stack<Move> clonedMoves = new Stack<>();
         clonedMoves.addAll(this.moves);
         return clonedMoves;
+    }
+
+    /**
+     * Method getFormattedTime.
+     * @return elapsedTime The formatted time
+     */
+    public static String getFormattedTime() {
+        elapsedTime = formatMillis(System.currentTimeMillis() - Long.parseLong(startTime));
+        return elapsedTime;
+    }
+
+    /**
+     * Method formatMillis.
+     * @param millis The milliseconds
+     * @return Returns the formatted time
+     */
+    public static String formatMillis(final long millis) {
+        long totalSeconds = millis / MILLIS;
+        long seconds = totalSeconds % SECONDS;
+        long totalMinutes = totalSeconds / SECONDS;
+        long minutes = totalMinutes % SECONDS;
+        long hours = totalMinutes / SECONDS;
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
     }
 }
