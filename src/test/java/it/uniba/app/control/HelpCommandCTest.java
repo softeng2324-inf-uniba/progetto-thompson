6be3dd32a6e0b584c42d1ad33722fc868ca6cf4f
frasga <s.francesco297@gmail.com>
2024-06-08 16:28:19 +0200

    ref(HelpCommandCTest): Aggiunta test helpCommandCInvalidArgumentsTest

--- src/test/java/it/uniba/app/control/HelpCommandCTest.java
+++ src/test/java/it/uniba/app/control/HelpCommandCTest.java
@@ -1,5 +1,6 @@
 package it.uniba.app.control;
 import it.uniba.app.Thompson.game.control.CommandC;
+import it.uniba.app.Thompson.game.control.ExitCommandC;
 import it.uniba.app.Thompson.game.control.HelpCommandC;
 import it.uniba.app.Thompson.game.error.InvalidArguments;
 import it.uniba.app.Thompson.game.util.CommandStatus;
@@ -14,6 +15,15 @@ class HelpCommandCTest {
     void helpCommandCTest() throws InvalidArguments {
         CommandC helpCommandC = HelpCommandC.getInstance();
         Assertions.assertEquals(helpCommandC.executeCommand(), CommandStatus.SUCCESSFUL, "HelpCommandCTest");
+    }
+
+    @Test
+    @DisplayName("HelpCommandCInvalidArgumentsTest")
+    void helpCommandCInvalidArgumentsTest() {
+        CommandC helpCommandC = ExitCommandC.getInstance();
+        String[] dummyArgs = {"dummy1", "dummy2", "dummy3"};
+
+        Assertions.assertThrows(InvalidArguments.class, () -> helpCommandC.executeCommand(dummyArgs), "Invalid number of arguments");
     }
 
 }
