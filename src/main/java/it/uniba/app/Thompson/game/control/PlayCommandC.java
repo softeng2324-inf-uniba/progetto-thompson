dd6f9f386ad5e1640ea9f1cd75a5e89dea34617e
f1r3k3rn <cronanacron@gmail.com>
2024-06-08 11:46:53 +0200

    fix(PlayCommand): Modificato tipo di accesso

--- src/main/java/it/uniba/app/Thompson/game/control/PlayCommandC.java
+++ src/main/java/it/uniba/app/Thompson/game/control/PlayCommandC.java
@@ -67,7 +67,7 @@ public final class PlayCommandC extends CommandC {
      * @return Returns the status of the command
      */
     @Override
-    CommandStatus executeCommand(final String... args) {
+    public CommandStatus executeCommand(final String... args) {
         if (MainControl.getMatch() == null) {
             MainControl.initMatch();
             CommunicateInteractionMessagesB.printTitle("TAVOLIERE INIZIALE");
