0927a851a489227213be9d42141ba3e143d68249
f1r3k3rn <cronanacron@gmail.com>
2024-06-08 12:53:54 +0200

    feat(TimeCommandCtest): Aggiunta classe di testing per il comando "/tempo"

---
+++ src/test/java/it/uniba/app/control/TimeCommandCTest.java
@@ -0,0 +1,30 @@
+package it.uniba.app.control;
+import it.uniba.app.Thompson.game.control.CommandC;
+import it.uniba.app.Thompson.game.control.MainControl;
+import it.uniba.app.Thompson.game.control.TimeCommandC;
+import it.uniba.app.Thompson.game.error.InvalidArguments;
+import it.uniba.app.Thompson.game.util.CommandStatus;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.lang.Thread;
+
+public class TimeCommandCTest {
+
+    @BeforeAll
+    static void setUp() {
+        MainControl.initMatch();
+        System.out.println("Setup completed for TimeCommandCTest");
+    }
+
+    @Test
+    @DisplayName("TimeCommandCTest")
+    void timeCommandCTest() throws InterruptedException, InvalidArguments {
+        System.out.println("Starting TimeCommandCTest");
+        CommandC timeCommandC = TimeCommandC.getInstance();
+        Thread.sleep(5000);
+        Assertions.assertEquals(timeCommandC.executeCommand(), CommandStatus.SUCCESSFUL, "TimeCommandCTest");
+    }
+
+}
