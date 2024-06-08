afcc5c56e3ce256c9446a6388beda80b1f002e79
f1r3k3rn <cronanacron@gmail.com>
2024-06-08 17:51:55 +0200

    feat(BoardCommandCTest): Aggiunta classe di testing per la classe BoardCommandCTest

---
+++ src/test/java/it/uniba/app/control/BoardCommandCTest.java
@@ -0,0 +1,38 @@
+package it.uniba.app.control;
+
+import it.uniba.app.Thompson.game.control.BoardCommandC;
+import it.uniba.app.Thompson.game.control.CommandC;
+import it.uniba.app.Thompson.game.control.MainControl;
+import it.uniba.app.Thompson.game.control.PlayCommandC;
+import it.uniba.app.Thompson.game.error.InvalidArguments;
+import it.uniba.app.Thompson.game.util.CommandStatus;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BoardCommandCTest {
+
+    @Test
+    @DisplayName("BoardCommandCTest: print board when the game hasn't started yet")
+    void boardCommandCTest() throws InvalidArguments {
+        BoardCommandC boardCommandC = BoardCommandC.getInstance();
+        Assertions.assertEquals(boardCommandC.executeCommand(), CommandStatus.SUCCESSFUL, "BoardCommandCTest");
+    }
+
+    @Test
+    @DisplayName("BoardCommandCTest: print board when the game has started")
+    void boardCommandCTestOnGoing() throws InvalidArguments {
+        MainControl.initMatch();
+        BoardCommandC boardCommandC = BoardCommandC.getInstance();
+        Assertions.assertEquals(boardCommandC.executeCommand(), CommandStatus.SUCCESSFUL, "BoardCommandCTest");
+    }
+
+    @Test
+    @DisplayName("BoardCommandCTest: invalid number of arguments")
+    void PlayCommandCInvalidArgumentsTest() {
+        CommandC boardCommandC = BoardCommandC.getInstance();
+        String[] dummyArgs = {"dummy1", "dummy2", "dummy3"};
+
+        Assertions.assertThrows(InvalidArguments.class, () -> boardCommandC.executeCommand(dummyArgs), "Invalid number of arguments");
+    }
+}
