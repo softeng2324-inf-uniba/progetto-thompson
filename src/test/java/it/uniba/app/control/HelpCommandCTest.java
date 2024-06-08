cc029faa67a354e3c09660af9860acadabffd32c
f1r3k3rn <cronanacron@gmail.com>
2024-06-08 12:55:02 +0200

    feat(HelpCommandCtest): Aggiunta classe di testing per la classe HelpCommandCTest

---
+++ src/test/java/it/uniba/app/control/HelpCommandCTest.java
@@ -0,0 +1,19 @@
+package it.uniba.app.control;
+import it.uniba.app.Thompson.game.control.CommandC;
+import it.uniba.app.Thompson.game.control.HelpCommandC;
+import it.uniba.app.Thompson.game.error.InvalidArguments;
+import it.uniba.app.Thompson.game.util.CommandStatus;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class HelpCommandCTest {
+
+    @Test
+    @DisplayName("HelpCommandTest")
+    void helpCommandCTest() throws InvalidArguments {
+        CommandC helpCommandC = HelpCommandC.getInstance();
+        Assertions.assertEquals(helpCommandC.executeCommand(), CommandStatus.SUCCESSFUL, "HelpCommandCTest");
+    }
+
+}
