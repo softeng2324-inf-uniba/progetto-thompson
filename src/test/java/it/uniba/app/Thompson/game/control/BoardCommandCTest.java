7caeb9e00e2f5f987a2597ce61af6022c60daf5c
Pascoooo <pacucciniko@gmail.com>
2024-06-09 21:56:15 +0200

    fix(BoardCommandCTest): Rimozione import superflui per Checkstyle

--- src/test/java/it/uniba/app/Thompson/game/control/BoardCommandCTest.java
+++ src/test/java/it/uniba/app/Thompson/game/control/BoardCommandCTest.java
@@ -1,7 +1,4 @@
-package it.uniba.app.control;
-import it.uniba.app.Thompson.game.control.BoardCommandC;
-import it.uniba.app.Thompson.game.control.CommandC;
-import it.uniba.app.Thompson.game.control.MainControl;
+package it.uniba.app.Thompson.game.control;
 import it.uniba.app.Thompson.game.error.InvalidArguments;
 import it.uniba.app.Thompson.game.util.CommandStatus;
 import org.junit.jupiter.api.Assertions;
@@ -29,10 +26,11 @@ class BoardCommandCTest {
 
     @Test
     @DisplayName("BoardCommandCTest: invalid number of arguments")
-    void PlayCommandCInvalidArgumentsTest() {
+    void playCommandCInvalidArgumentsTest() {
         CommandC boardCommandC = BoardCommandC.getInstance();
         String[] dummyArgs = {"dummy1", "dummy2", "dummy3"};
 
-        Assertions.assertThrows(InvalidArguments.class, () -> boardCommandC.executeCommand(dummyArgs), "Invalid number of arguments");
+        Assertions.assertThrows(InvalidArguments.class, () -> boardCommandC.executeCommand(dummyArgs),
+        "Invalid number of arguments");
     }
 }
