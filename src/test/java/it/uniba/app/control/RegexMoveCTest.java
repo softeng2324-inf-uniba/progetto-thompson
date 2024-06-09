0898d5c366b5c4d08f6632489930d02d55cf3511
f1r3k3rn <cronanacron@gmail.com>
2024-06-09 15:57:38 +0200

    feat(RegexMoveCTest): Aggiunta classe di test RegexMoveCTest per la classe RegexMoveC

---
+++ src/test/java/it/uniba/app/control/RegexMoveCTest.java
@@ -0,0 +1,51 @@
+package it.uniba.app.control;
+
+import it.uniba.app.Thompson.game.control.RegexMoveC;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RegexMoveCTest {
+
+    @Test
+    @DisplayName("RegexMoveCTest: test the regex for a valid block input")
+    void regexMoveCTestBlockTrue() {
+        RegexMoveC regexMoveC = new RegexMoveC();
+        Assertions.assertTrue(regexMoveC.controlInputBlock("d4"), "RegexMoveCTest");
+    }
+
+    @Test
+    @DisplayName("RegexMoveCTest: test the regex for an invalid block input")
+    void regexMoveCTestBlockFalse() {
+        RegexMoveC regexMoveC = new RegexMoveC();
+        Assertions.assertFalse(regexMoveC.controlInputBlock("a8"), "RegexMoveCTest");
+    }
+
+    @Test
+    @DisplayName("RegexMoveCTest: test the regex for a valid move input")
+    void regexMoveCTestMoveTrue() {
+        RegexMoveC regexMoveC = new RegexMoveC();
+        Assertions.assertTrue(regexMoveC.controlInputMovement("d4-d5"), "RegexMoveCTest");
+    }
+
+    @Test
+    @DisplayName("RegexMoveCTest: test the regex for a valid move input")
+    void regexMoveCTestMoveFalse() {
+        RegexMoveC regexMoveC = new RegexMoveC();
+        Assertions.assertFalse(regexMoveC.controlInputMovement("d4-d9"), "RegexMoveCTest");
+    }
+
+    @Test
+    @DisplayName("RegexMoveCTest: test the regex for a valid generic coordinate input")
+    void regexMoveCTestGenericCoordinateTrue() {
+        RegexMoveC regexMoveC = new RegexMoveC();
+        Assertions.assertTrue(regexMoveC.isGenericCoordinate("d4-d5"), "RegexMoveCTest");
+    }
+
+    @Test
+    @DisplayName("RegexMoveCTest: test the regex for an invalid generic coordinate input")
+    void regexMoveCTestGenericCoordinateFalse() {
+        RegexMoveC regexMoveC = new RegexMoveC();
+        Assertions.assertFalse(regexMoveC.isGenericCoordinate("d4-%9"), "RegexMoveCTest");
+    }
+}
