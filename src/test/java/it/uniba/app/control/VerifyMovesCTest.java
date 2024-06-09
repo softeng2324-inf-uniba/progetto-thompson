8ecc0d1866bd7512b11c10a7ad674533a4478203
f1r3k3rn <cronanacron@gmail.com>
2024-06-09 18:08:24 +0200

    fix(VerifyMovesCTest): Risoluzione ruleset di PMD

--- src/test/java/it/uniba/app/control/VerifyMovesCTest.java
+++ src/test/java/it/uniba/app/control/VerifyMovesCTest.java
@@ -3,7 +3,6 @@ import it.uniba.app.Thompson.game.control.MainControl;
 import it.uniba.app.Thompson.game.control.VerifyMovesC;
 import it.uniba.app.Thompson.game.entity.BoardE;
 import it.uniba.app.Thompson.game.entity.TileE;
-import it.uniba.app.Thompson.game.error.InvalidArguments;
 import it.uniba.app.Thompson.game.util.Coordinate;
 import it.uniba.app.Thompson.game.util.PawnFigure;
 import org.junit.jupiter.api.Assertions;
@@ -17,8 +16,7 @@ class VerifyMovesCTest {
         MainControl.initMatch();
         BoardE board = MainControl.getMatch().getBoard();
 
-        VerifyMovesC.verifyMovesSinglePawn(board, new Coordinate(0,0), new Coordinate(1,1));
-        Assertions.assertTrue(VerifyMovesC.verifyMovesSinglePawn(board, new Coordinate(0,0), new Coordinate(1,1)), "VerifyMovesCTest");
+        Assertions.assertTrue(VerifyMovesC.movesSinglePawn(board, new Coordinate(0,0), new Coordinate(0,1)), "VerifyMovesCTest");
         MainControl.removeMatch();
     }
 
@@ -28,25 +26,25 @@ class VerifyMovesCTest {
         MainControl.initMatch();
         BoardE board = MainControl.getMatch().getBoard();
 
-        Assertions.assertFalse(VerifyMovesC.verifyMovesSinglePawn(board, new Coordinate(0,0), new Coordinate(1,8)), "VerifyMovesCTest");
+        Assertions.assertFalse(VerifyMovesC.movesSinglePawn(board, new Coordinate(0,0), new Coordinate(1,8)), "VerifyMovesCTest");
         MainControl.removeMatch();
     }
 
     @Test
     @DisplayName("VerifyMovesCTest: test the verification of a move on a blocked tile")
-    void verifyMovesCtestBlockedTile() throws InvalidArguments {
+    void verifyMovesCtestBlockedTile()  {
         BoardE board = new BoardE(true);
         board.setTile(new Coordinate(0,1), new TileE(0,1,true));
 
         MainControl.initMatch();
         MainControl.setMatchBoard(board);
-        Assertions.assertFalse(VerifyMovesC.verifyMovesSinglePawn(board, new Coordinate(0,0), new Coordinate(0,1)), "VerifyMovesCTest");
+        Assertions.assertFalse(VerifyMovesC.movesSinglePawn(board, new Coordinate(0,0), new Coordinate(0,1)), "VerifyMovesCTest");
         MainControl.removeMatch();
     }
 
     @Test
     @DisplayName("VerifyMovesCTest: test the verification of a move on a occupied tile")
-    void verifyMovesCtestOccupiedTile() throws InvalidArguments {
+    void verifyMovesCtestOccupiedTile() {
         BoardE board = new BoardE(true);
         TileE tile = new TileE(0,1);
         tile.placePawn(PawnFigure.BLACK_PAWN);
@@ -54,7 +52,7 @@ class VerifyMovesCTest {
 
         MainControl.initMatch();
         MainControl.setMatchBoard(board);
-        Assertions.assertFalse(VerifyMovesC.verifyMovesSinglePawn(board, new Coordinate(0,0), new Coordinate(0,1)), "VerifyMovesCTest");
+        Assertions.assertFalse(VerifyMovesC.movesSinglePawn(board, new Coordinate(0,0), new Coordinate(0,1)), "VerifyMovesCTest");
         MainControl.removeMatch();
     }
 }
