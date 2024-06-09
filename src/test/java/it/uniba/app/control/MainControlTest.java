cac9d8b3c8c57e8cb296fd09a048ef069fa97b45
frasga <s.francesco297@gmail.com>
2024-06-09 16:17:47 +0200

    ref(MainControlTest): Creazione pushMoveTest

--- src/test/java/it/uniba/app/control/MainControlTest.java
+++ src/test/java/it/uniba/app/control/MainControlTest.java
@@ -1,6 +1,7 @@
 package it.uniba.app.control;
 import it.uniba.app.Thompson.game.control.MainControl;
-import it.uniba.app.Thompson.game.error.InvalidArguments;
+import it.uniba.app.Thompson.game.entity.MoveE;
+import it.uniba.app.Thompson.game.util.Coordinate;
 import it.uniba.app.Thompson.game.util.PawnFigure;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -25,5 +26,19 @@ class MainControlTest {
         Assertions.assertNotEquals(MainControl.getMatch(), null, "MainControlTest");
     }
 
+    @Test
+    @DisplayName("MainControlTest : push a move into main match")
+    void pushMoveTest() {
+        MainControl.initMatch();
+        MainControl.pushMoveQueue(new MoveE((new Coordinate(1,1)), (new Coordinate(2,2))));
+
+        Assertions.assertEquals(MainControl.getMatch().getMoves().toArray().length, 1, "MainControlTest");
+    }
+
+
+//
+//    @Test
+//    @DisplayName("MainControlTest : vince il bianco e perde il ner")
+//
 
 }
