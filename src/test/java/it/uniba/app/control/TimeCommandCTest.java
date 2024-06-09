61639f38580fda0a7c281aba46dbe612287f3e22
f1r3k3rn <cronanacron@gmail.com>
2024-06-09 15:51:49 +0200

    fix(TimeCommandCTest): Cambio modificatore di accesso

--- src/test/java/it/uniba/app/control/TimeCommandCTest.java
+++ src/test/java/it/uniba/app/control/TimeCommandCTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.lang.Thread;
 
-public class TimeCommandCTest {
+class TimeCommandCTest {
 
     @BeforeAll
     static void setUp() {
