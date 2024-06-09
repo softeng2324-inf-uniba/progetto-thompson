dde266011d46b99154fadff88a51146b08e31331
f1r3k3rn <cronanacron@gmail.com>
2024-06-09 16:00:11 +0200

    fix(ExitCommandCTest): Aggiustato Modificatore di visibilità

--- src/test/java/it/uniba/app/control/ExitCommandCTest.java
+++ src/test/java/it/uniba/app/control/ExitCommandCTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class ExitCommandCTest {
+class ExitCommandCTest {
 
     @BeforeAll
     static void setUp() {
