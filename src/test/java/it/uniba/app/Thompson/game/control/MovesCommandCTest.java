59c48c175e70c92fd9101566d5545ced8137e198
Pascoooo <pacucciniko@gmail.com>
2024-06-09 21:47:29 +0200

    fix(MoveETest): Spostato package e aggiunte costanti per risolvere problemi di checkstyle

--- src/test/java/it/uniba/app/Thompson/game/control/MovesCommandCTest.java
+++ src/test/java/it/uniba/app/Thompson/game/control/MovesCommandCTest.java
@@ -1,6 +1,4 @@
-package it.uniba.app.control;
-import it.uniba.app.Thompson.game.control.MainControl;
-import it.uniba.app.Thompson.game.control.MovesCommandC;
+package it.uniba.app.Thompson.game.control;
 import it.uniba.app.Thompson.game.entity.MoveE;
 import it.uniba.app.Thompson.game.error.InvalidArguments;
 import it.uniba.app.Thompson.game.util.CommandStatus;
@@ -8,9 +6,10 @@ import it.uniba.app.Thompson.game.util.Coordinate;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-
 class MovesCommandCTest {
+    private static final int CONSTANT_SIX = 6;
+    private static final int CONSTANT_FIVE = 5;
+    private static final int CONSTANT_THREE = 3;
 
     @Test
     @DisplayName("MovesCommandCTest: test the command if the game has started")
@@ -36,7 +35,8 @@ class MovesCommandCTest {
         MovesCommandC movesCommandC = MovesCommandC.getInstance();
         String[] dummyArgs = {"dummy1", "dummy2", "dummy3"};
 
-        Assertions.assertThrows(InvalidArguments.class, () -> movesCommandC.executeCommand(dummyArgs), "Invalid number of arguments");
+        Assertions.assertThrows(InvalidArguments.class, () -> movesCommandC.executeCommand(dummyArgs),
+                "Invalid number of arguments");
     }
 
     @Test
@@ -45,8 +45,8 @@ class MovesCommandCTest {
         MainControl.initMatch();
         MovesCommandC movesCommandC = MovesCommandC.getInstance();
         MainControl.pushMoveQueue(new MoveE(new Coordinate(0, 0), new Coordinate(1, 1)));
-        MainControl.pushMoveQueue(new MoveE(new Coordinate(0, 6), new Coordinate(0, 5)));
-        MainControl.pushMoveQueue(new MoveE(new Coordinate(1, 1), new Coordinate(3, 3)));
+        MainControl.pushMoveQueue(new MoveE(new Coordinate(0, CONSTANT_SIX), new Coordinate(0, CONSTANT_FIVE)));
+        MainControl.pushMoveQueue(new MoveE(new Coordinate(1, 1), new Coordinate(CONSTANT_THREE, CONSTANT_THREE)));
 
         Assertions.assertEquals(movesCommandC.executeCommand(), CommandStatus.SUCCESSFUL, "MovesCommandCTest");
         MainControl.removeMatch();
